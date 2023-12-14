2979aeb3f2047a0d3ba69c9bd7265cf6e55f831f
Nichlas Handstedt <nichlas.handstedt@scania.com>
2023-12-15 00:40:32 +0100

    configure logging

---
+++ storeapi/logging_conf.py
@@ -0,0 +1,59 @@
+from logging.config import dictConfig
+
+from storeapi.config import DevConfig, config
+
+
+def configure_logging() -> None:
+    dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "filters": {
+                "correlation_id": {
+                    "()": "asgi_correlation_id.CorrelationIdFilter",
+                    "uuid_length": 8 if isinstance(config, DevConfig) else 32,
+                    "default_value": "-",
+                }
+            },
+            "formatters": {
+                "console": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "(%(correlation_id)s) %(name)s:%(lineno)d - %(message)s",
+                },
+                "file": {
+                    "class": "pythonjsonlogger.jsonlogger.JsonFormatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s %(msecs)03d %(levelname)-8s %(correlation_id)s %(name)s %(lineno)d %(message)s",
+                },
+            },
+            "handlers": {
+                "default": {
+                    "class": "rich.logging.RichHandler",
+                    "level": "DEBUG",
+                    "formatter": "console",
+                    "filters": ["correlation_id"],
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "storeapi.log",
+                    "maxBytes": 1024 * 1024,  # 1MB
+                    "backupCount": 2,
+                    "encoding": "utf8",
+                    "filters": ["correlation_id"],
+                },
+            },
+            "loggers": {
+                "uvicorn": {"handlers": ["default", "rotating_file"], "level": "INFO"},
+                "storeapi": {
+                    "handlers": ["default", "rotating_file"],
+                    "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
+                    "propagate": False,
+                },
+                "databases": {"handlers": ["default"], "level": "WARNING"},
+                "aiosqlite": {"handlers": ["default"], "level": "WARNING"},
+            },
+        }
+    )
--- storeapi/main.py
+++ storeapi/main.py
@@ -1,18 +1,34 @@
+import logging
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from asgi_correlation_id import CorrelationIdMiddleware
+from fastapi import FastAPI, HTTPException
+from fastapi.exception_handlers import http_exception_handler
 
 from storeapi.database import database
+from storeapi.logging_conf import configure_logging
 from storeapi.routers.post import router as post_router
+
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    configure_logging()
     await database.connect()
     yield
     await database.disconnect()
 
+
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(CorrelationIdMiddleware)
 
 
 app.include_router(post_router)
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handle_logging(request, exc):
+    logger.error(f"HTTPException: {exc.status_code} {exc.detail}")
+    return await http_exception_handler(request, exc)
